7cae8a08f709927b38e10621a1a41c531f728755
Andy Sowar <andy.sowar@gmail.com>
2015-07-19 13:59:11 -0600

    Added test for adding a second car
    [#99360054]

--- spec/features/creating_cars_spec.rb
+++ spec/features/creating_cars_spec.rb
@@ -23,4 +23,40 @@ feature 'Creating Cars' do
       expect(page).to have_content('2300')
     end
   end
+
+  scenario 'can add a second car' do
+    visit '/'
+
+    click_link 'New Car'
+
+    fill_in 'Make', with: 'Ford'
+    fill_in 'Model', with: 'Mustang'
+    fill_in 'Year', with: '1968'
+    fill_in 'Price', with: '2300'
+
+    click_button 'Create Car'
+
+    click_link 'New Car'
+
+    fill_in 'Make', with: 'Subaru'
+    fill_in 'Model', with: 'Impreza'
+    fill_in 'Year', with: '2007'
+    fill_in 'Price', with: '12500'
+
+    click_button 'Create Car'
+
+    within ('#cars') do
+      expect(page).to have_content('Ford')
+      expect(page).to have_content('Mustang')
+      expect(page).to have_content('1968')
+      expect(page).to have_content('2300')
+
+      expect(page).to have_content('Subaru')
+      expect(page).to have_content('Impreza')
+      expect(page).to have_content('2007')
+      expect(page).to have_content('12500')
+    end
+  end
 end
+
+
